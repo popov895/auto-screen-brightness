8fda5d9ded422ab7b56b8cb959f8b0a7f86c0255
Eugene Popov <popov895@ukr.net>
2024-09-24 16:59:40 +0300

    Code improvements

--- extension.js
+++ extension.js
@@ -1,14 +1,18 @@
 'use strict';
 
 import Gio from 'gi://Gio';
-import GLib from 'gi://GLib';
 import { loadInterfaceXML } from 'resource:///org/gnome/shell/misc/fileUtils.js';
 
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { Preferences } from './lib/preferences.js';
 
-const brightnessInterfaceXml = loadInterfaceXML(`org.gnome.SettingsDaemon.Power.Screen`);
-const powerManagerInterfaceXml = loadInterfaceXML(`org.freedesktop.UPower`);
+const BrightnessProxy = Gio.DBusProxy.makeProxyWrapper(
+    loadInterfaceXML(`org.gnome.SettingsDaemon.Power.Screen`)
+);
+
+const PowerManagerProxy = Gio.DBusProxy.makeProxyWrapper(
+    loadInterfaceXML(`org.freedesktop.UPower`)
+);
 
 export default class extends Extension {
     enable() {
@@ -24,15 +28,16 @@ export default class extends Extension {
             }
         }, this);
 
-        const brightnessInterfaceInfo = Gio.DBusInterfaceInfo.new_for_xml(brightnessInterfaceXml);
-        this._brightnessProxy = new Gio.DBusProxy({
-            g_bus_type: Gio.BusType.SESSION,
-            g_name: `org.gnome.SettingsDaemon.Power`,
-            g_object_path: `/org/gnome/SettingsDaemon/Power`,
-            g_interface_info: brightnessInterfaceInfo,
-            g_interface_name: brightnessInterfaceInfo.name,
-            g_flags: Gio.DBusProxyFlags.NONE,
-        });
+        this._brightnessProxy = new BrightnessProxy(
+            Gio.DBus.session,
+            `org.gnome.SettingsDaemon.Power`,
+            `/org/gnome/SettingsDaemon/Power`,
+            (...[, error]) => {
+                if (error) {
+                    this._logError(error);
+                }
+            }
+        );
         this._brightnessProxy.connectObject(`g-properties-changed`, (...[, properties]) => {
             if (properties.lookup_value(`Brightness`, null) !== null) {
                 this._brightnessProxy.disconnectObject(this);
@@ -40,39 +45,21 @@ export default class extends Extension {
             }
         }, this);
 
-        const powerManagerInterfaceInfo = Gio.DBusInterfaceInfo.new_for_xml(powerManagerInterfaceXml);
-        this._powerManagerProxy = new Gio.DBusProxy({
-            g_bus_type: Gio.BusType.SYSTEM,
-            g_name: `org.freedesktop.UPower`,
-            g_object_path: `/org/freedesktop/UPower`,
-            g_interface_info: powerManagerInterfaceInfo,
-            g_interface_name: powerManagerInterfaceInfo.name,
-            g_flags: Gio.DBusProxyFlags.NONE,
-        });
+        this._powerManagerProxy = new PowerManagerProxy(
+            Gio.DBus.system,
+            `org.freedesktop.UPower`,
+            `/org/freedesktop/UPower`,
+            (...[, error]) => {
+                if (error) {
+                    this._logError(error);
+                }
+            }
+        );
         this._powerManagerProxy.connectObject(`g-properties-changed`, (...[, properties]) => {
             if (properties.lookup_value(`OnBattery`, null) !== null) {
                 this._updateScreenBrightness();
             }
         }, this);
-
-        this._brightnessProxy.init_async(GLib.PRIORITY_DEFAULT, null, (proxy, result) => {
-            try {
-                if (!proxy.init_finish(result)) {
-                    throw new Error(`Failed to connect to the ${proxy.g_interface_name} D-Bus interface`);
-                }
-            } catch (error) {
-                this._logError(error);
-            }
-        });
-        this._powerManagerProxy.init_async(GLib.PRIORITY_DEFAULT, null, (proxy, result) => {
-            try {
-                if (!proxy.init_finish(result)) {
-                    throw new Error(`Failed to connect to the ${proxy.g_interface_name} D-Bus interface`);
-                }
-            } catch (error) {
-                this._logError(error);
-            }
-        });
     }
 
     disable() {
